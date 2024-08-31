dd7c8eb5f4b916f101f50cce9995f4c3ccaa3187
Sarawut Kaewyana <162553652+Saraeutsza@users.noreply.github.com>
2024-09-01 05:19:20 +0700

    Initialize alec/contract

---
+++ src/constants.ts
@@ -0,0 +1,30 @@
+export const BASE_SEPOLIA_CHAIN_ID = 84532;
+
+export const clickContractAddress =
+  '0x67c97D1FB8184F038592b2109F854dfb09C77C75';
+
+export const clickContractABI = [
+  {
+    type: 'function',
+    name: 'click',
+    inputs: [],
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+] as const;
+
+export const collectionAddress = '0xd6915560d3bb24aec04dc42ef409921ed1931510';
+export const comment = 'testing';
+export const mintContractAddress = '0x11b4128A343d9aB02bED83050c952f383EAb6B24';
+export const mintABI = [
+  {
+    inputs: [],
+    name: 'mint',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+] as const;
+export const mintReferral = '0x0000000000000000000000000000000000000000';
+export const quantity = '1';
+export const tokenId = '1';
